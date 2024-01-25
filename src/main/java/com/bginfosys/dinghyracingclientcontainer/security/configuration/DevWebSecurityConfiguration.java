7e8924e7a45c68d3a8b642855aa2a0829be9e52b
Ben Greener <53060452+bengreener@users.noreply.github.com>
2024-01-25 17:58:08 +0000

    create new configuration for web security using h2 database and remove h2 console configuration from default web security configuration

--- src/main/java/com/bginfosys/dinghyracingclientcontainer/security/configuration/DevWebSecurityConfiguration.java
+++ src/main/java/com/bginfosys/dinghyracingclientcontainer/security/configuration/DevWebSecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.bginfosys.dinghyracingclientcontainer.security.configuration;
 
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 import org.springframework.context.annotation.Bean;
@@ -8,19 +9,29 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
-@Profile("!dev")
-public class WebSecurityConfiguration {
+@Profile("dev")
+public class DevWebSecurityConfiguration {
 		
 	@Bean
 	protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.authorizeHttpRequests(authorize -> authorize
+		    .requestMatchers(toH2Console()).permitAll()
 		    .requestMatchers("/**").authenticated()
 	    )
 	    .formLogin(withDefaults());
+		http.csrf((csrf) -> {
+			csrf.csrfTokenRepository(new HttpSessionCsrfTokenRepository());
+			csrf.ignoringRequestMatchers(toH2Console());
+		});
+		http.headers(headers -> 
+	    	headers.frameOptions(frameOptions -> 
+	    		frameOptions.sameOrigin()
+			)
+    	);
 		return http.build();
 	}
 }
