eaa14eb842b596222adde9372dc0fc02d737cf05
Ben Greener <53060452+bengreener@users.noreply.github.com>
2024-01-25 12:25:43 +0000

    initial commit

---
+++ src/main/java/com/bginfosys/dinghyracingclientcontainer/DinghyRacingClientContainerApplication.java
@@ -0,0 +1,13 @@
+package com.bginfosys.dinghyracingclientcontainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinghyRacingClientContainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinghyRacingClientContainerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bginfosys/dinghyracingclientcontainer/DinghyRacingClientContainerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bginfosys.dinghyracingclientcontainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinghyRacingClientContainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
