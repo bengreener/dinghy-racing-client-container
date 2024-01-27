f4f047f41b7c62c1e8a19b472df146ebb0af17a3
Ben Greener <53060452+bengreener@users.noreply.github.com>
2024-01-27 20:30:13 +0000

    update mysql database scripts

--- sql_scripts/mysql_create-dinghy-racing-client-database.sql
+++ sql_scripts/mysql_create-dinghy-racing-client-database.sql
@@ -11,3 +11,12 @@ CREATE TABLE `user` (
     password CHAR(60) NOT NULL
 ) engine=InnoDB;
 
+CREATE TABLE role (
+	id BIGINT NOT NULL, 
+    name VARCHAR(255) NOT NULL
+) engine=InnoDB;
+
+CREATE TABLE `user_roles` (
+	`user_id` BIGINT NOT NULL,
+    roles_id BIGINT NOT NULL
+) engine=InnoDB;
