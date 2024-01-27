31786832d2b68b57ba603ad2ec6692fb37a6a7be
Ben Greener <53060452+bengreener@users.noreply.github.com>
2024-01-27 20:22:54 +0000

    add authentication controller to provide roles

--- src/main/java/com/bginfosys/dinghyracingclientcontainer/security/facade/AuthenticationFacade.java
+++ src/main/java/com/bginfosys/dinghyracingclientcontainer/security/facade/AuthenticationFacade.java
@@ -1,5 +1,9 @@
 package com.bginfosys.dinghyracingclientcontainer.security.facade;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -7,7 +11,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 @Component
-public class AuthenticationFacade {
+public class AuthenticationFacade {	
 
 	public Authentication getAuthentication() {
 		return SecurityContextHolder.getContext().getAuthentication();
@@ -17,8 +21,14 @@ public class AuthenticationFacade {
 		return (UserDetails) getAuthentication().getPrincipal();
 	}
 
+	public List<String> principalRoles() {
+		return getPrincipal().getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList());
+	}
+	
 	public String principalRolesToString() {
 		return getPrincipal().getAuthorities().stream().map(GrantedAuthority::getAuthority).reduce("", (string1, string2) -> string1 != "" ? string1 + ";" + string2 : string2);
 	}
+	
+	
 
 }
---
+++ src/main/java/com/bginfosys/dinghyracingclientcontainer/security/model/Roles.java
@@ -0,0 +1,19 @@
+package com.bginfosys.dinghyracingclientcontainer.security.model;
+
+import java.util.List;
+
+public class Roles {
+
+	private List<String> roles;
+	
+	public Roles() {};
+	
+	public List<String> getRoles() {
+		return this.roles;
+	}
+	
+	public void setRoles(List<String> roles) {
+		this.roles = roles;
+	}
+}
+ 
