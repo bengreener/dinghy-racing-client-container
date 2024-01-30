52e2bf6596e9ce4ca3a963ef092f72e6620d4d75
Ben Greener <53060452+bengreener@users.noreply.github.com>
2024-01-30 15:45:55 +0000

    remove OOD and add CO users to dev data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,7 +4,7 @@ insert into role (id, name) values (3, 'COMPETITOR');
 
 insert into "user" (id, username, password) values (4, 'SS', '$2a$10$Gbp1XLmdhelQpMvC3J8heugmTWKVvfsy9UfKqpZ6nxzqM3ySG7evi'); -- password: ss
 insert into "user" (id, username, password) values (5, 'RO', '$2a$10$oxGe8lZ4OR7kEAng8zgfYus9ofN6aU/iSq8tPiboNngVp9Vq1PdFG'); -- password: ro
-insert into "user" (id, username, password) values (6, 'OOD', '$2a$10$c7iYFL4UuomBGM0a4V8po.kFcNQ175H9y1r8VeiRzq4.xdQonyg.u'); -- password: ood
+insert into "user" (id, username, password) values (6, 'CO', '$2a$10$KO4dEs01J1wo/8tOBni0nuNFg8ZvRndf2uRY8OhQdCPdmgw0zh8x2'); -- password: co
 
 insert into "user_roles" ("user_id", roles_id) values (4, 1);
 insert into "user_roles" ("user_id", roles_id) values (4, 2);
