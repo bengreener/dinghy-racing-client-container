e0124f82e74f91c0be52b1c514bb626142cd8b76
Ben Greener <53060452+bengreener@users.noreply.github.com>
2024-02-27 18:29:05 +0000

    correct error using " instead of ` to wrap user in sql test data script

--- sql_scripts/mysql_setup-dinghy-racing-client-test-data.sql
+++ sql_scripts/mysql_setup-dinghy-racing-client-test-data.sql
@@ -12,7 +12,7 @@ INSERT INTO role (id, name) VALUES (3, 'COMPETITOR');
 
 INSERT INTO `user` (id, username, password) VALUES (4, 'SS', '$2a$10$Gbp1XLmdhelQpMvC3J8heugmTWKVvfsy9UfKqpZ6nxzqM3ySG7evi'); -- password: ss
 INSERT INTO `user` (id, username, password) VALUES (5, 'RO', '$2a$10$oxGe8lZ4OR7kEAng8zgfYus9ofN6aU/iSq8tPiboNngVp9Vq1PdFG'); -- password: ro
-INSERT INTO "user" (id, username, password) VALUES (6, 'CO', '$2a$10$KO4dEs01J1wo/8tOBni0nuNFg8ZvRndf2uRY8OhQdCPdmgw0zh8x2'); -- password: co
+INSERT INTO `user` (id, username, password) VALUES (6, 'CO', '$2a$10$KO4dEs01J1wo/8tOBni0nuNFg8ZvRndf2uRY8OhQdCPdmgw0zh8x2'); -- password: co
 
 -- assign SS RACE_SCHEDULER
 INSERT INTO `user_roles` (`user_id`, roles_id) VALUES (4, 1); 
