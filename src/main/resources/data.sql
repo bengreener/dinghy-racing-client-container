e4e0a3fbc46ab5052eafd10b681e0c7a2299c96b
Ben Greener <53060452+bengreener@users.noreply.github.com>
2024-02-27 18:17:13 +0000

    update static resources

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,6 +7,6 @@ insert into "user" (id, username, password) values (5, 'RO', '$2a$10$oxGe8lZ4OR7
 insert into "user" (id, username, password) values (6, 'CO', '$2a$10$KO4dEs01J1wo/8tOBni0nuNFg8ZvRndf2uRY8OhQdCPdmgw0zh8x2'); -- password: co
 
 insert into "user_roles" ("user_id", roles_id) values (4, 1);
-insert into "user_roles" ("user_id", roles_id) values (4, 2);
+-- insert into "user_roles" ("user_id", roles_id) values (4, 2);
 insert into "user_roles" ("user_id", roles_id) values (5, 2);
 insert into "user_roles" ("user_id", roles_id) values (6, 3);
