8e4e7cf989abe5ffb3498b07932cd9d3e7cc445c
Ben Greener <53060452+bengreener@users.noreply.github.com>
2025-01-24 16:04:48 +0000

    remove files not required for build from build path locations

--- src/test/java/com/bginfosys/dinghyracingclientcontainer/remoteconections/web/controller/DinghyRacingPropertiesControllerTests.java
+++ src/test/java/com/bginfosys/dinghyracingclientcontainer/remoteconections/web/controller/DinghyRacingPropertiesControllerTests.java
@@ -25,12 +25,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.security.test.context.support.WithMockUser;
 
 import com.bginfosys.dinghyracingclientcontainer.remoteconnections.web.controller.DinghyRacingPropertiesController;
 
 @WebMvcTest(DinghyRacingPropertiesController.class)
+@ActiveProfiles("test")
 public class DinghyRacingPropertiesControllerTests {
 	
 	@Autowired
