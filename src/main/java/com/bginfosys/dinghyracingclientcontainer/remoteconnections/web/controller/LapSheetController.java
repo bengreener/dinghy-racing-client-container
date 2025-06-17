03b13fe267a2cd68306cdee06a7ba4dbd07817e8
Ben Greener <53060452+bengreener@users.noreply.github.com>
2025-06-17 13:43:17 +0100

    write race name out to lap sheet

--- src/main/java/com/bginfosys/dinghyracingclientcontainer/remoteconnections/web/controller/LapSheetController.java
+++ src/main/java/com/bginfosys/dinghyracingclientcontainer/remoteconnections/web/controller/LapSheetController.java
@@ -1,13 +1,23 @@
 package com.bginfosys.dinghyracingclientcontainer.remoteconnections.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.bginfosys.dinghyracingclientcontainer.remoteconnections.web.restclient.RestClient;
+import com.bginfosys.dinghyracingclientcontainer.remoteconnections.web.restclient.model.Race;
 
 @Controller
 public class LapSheetController {
 	
+	@Autowired
+	private RestClient restClient;
+		
 	@GetMapping("/lap-sheet") 
-	public String lapSheet() {
+	public String lapSheet(Model model) {
+		Race race = restClient.getRaceById(1L);
+		model.addAttribute("name", race.getName());
 		return "lap-sheet";
 	}
 
