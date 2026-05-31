bf1c9f58bd0fdc790524aa659db4bcf31284688a
Ben Greener <53060452+bengreener@users.noreply.github.com>
2026-05-31 10:48:25 +0100

    add web MVC configuration to reference a local.css stylesheet on /external/ path

---
+++ src/main/java/com/bginfosys/dinghyracingclientcontainer/DinghyRacingClientContainerConfigurer.java
@@ -0,0 +1,17 @@
+package com.bginfosys.dinghyracingclientcontainer;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class DinghyRacingClientContainerConfigurer implements WebMvcConfigurer {
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry
+			.addResourceHandler("/external/{:local.css}")
+			.addResourceLocations("file:./local/"); // can't pick up file from web application root; fails ResourceHandlerUtils.isResourceUnderLocation() check
+	}
+}
+	
